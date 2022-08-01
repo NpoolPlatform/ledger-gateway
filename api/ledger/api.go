845df95f8317cddc487fc818290f81bdbb359e84
Zhao KK <kikakkz@hotmail.com>
2022-08-01 05:00:24 +0000

    Add ledger gateway

--- api/ledger/api.go
+++ api/ledger/api.go
@@ -1,11 +1,9 @@
-package api
+package ledger
 
 import (
 	"context"
 
-	ledger "github.com/NpoolPlatform/message/npool/ledger/gw/v1"
-
-	ledger1 "github.com/NpoolPlatform/ledger-gateway/api/ledger"
+	"github.com/NpoolPlatform/message/npool/ledger/gw/v1/ledger"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
@@ -17,14 +15,10 @@ type Server struct {
 
 func Register(server grpc.ServiceRegistrar) {
 	ledger.RegisterGatewayServer(server, &Server{})
-	ledger1.Register(server)
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
 	if err := ledger.RegisterGatewayHandlerFromEndpoint(context.Background(), mux, endpoint, opts); err != nil {
-		return err
-	}
-	if err := ledger1.RegisterGateway(mux, endpoint, opts); err != nil {
 		return err
 	}
 	return nil
---
+++ api/ledger/ledger.go
@@ -0,0 +1,24 @@
+//nolint:nolintlint,dupl
+package ledger
+
+/*
+import (
+	"context"
+
+	converter "github.com/NpoolPlatform/ledger-gateway/pkg/converter/ledger"
+	commontracer "github.com/NpoolPlatform/ledger-gateway/pkg/tracer"
+	tracer "github.com/NpoolPlatform/ledger-gateway/pkg/tracer/ledger"
+
+	constant "github.com/NpoolPlatform/ledger-gateway/pkg/message/const"
+
+	"go.opentelemetry.io/otel"
+	scodes "go.opentelemetry.io/otel/codes"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/ledger/gw/v1/ledger"
+
+	"github.com/google/uuid"
+)
+*/
