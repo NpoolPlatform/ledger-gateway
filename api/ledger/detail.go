ad6fc13db59206e191afac9afaea11f89ad9d524
lidaqiang <dqli161@163.com>
2022-09-28 10:49:09 +0800

    Delete Println

--- api/ledger/detail.go
+++ api/ledger/detail.go
@@ -3,7 +3,6 @@ package ledger
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
@@ -64,7 +63,6 @@ func (s *Server) GetAppDetails(ctx context.Context, in *npool.GetAppDetailsReque
 	)
 	if err != nil {
 		logger.Sugar().Errorw("GetAppDetails", "error", err)
-		fmt.Println("error:", err)
 		return &npool.GetAppDetailsResponse{}, status.Error(codes.Internal, "fail get app details")
 	}
 
