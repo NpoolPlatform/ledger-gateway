f11f1f1d34215bc28bc51aca2b5a37e6ca0ecc37
lidaqiang <dqli161@163.com>
2022-09-28 12:03:51 +0800

    Detail Bugfix

--- pkg/ledger/detail.go
+++ pkg/ledger/detail.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 
 	commonpb "github.com/NpoolPlatform/message/npool"
+	appusermwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/user"
 	npool "github.com/NpoolPlatform/message/npool/ledger/gw/v1/ledger"
 	ledgermgrdetailpb "github.com/NpoolPlatform/message/npool/ledger/mgr/v1/ledger/detail"
 
+	usermwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
 	ledgermgrdetailcli "github.com/NpoolPlatform/ledger-manager/pkg/client/detail"
 	ledgermwcli "github.com/NpoolPlatform/ledger-middleware/pkg/client/ledger"
-
 	coininfopb "github.com/NpoolPlatform/message/npool/coininfo"
 	coininfocli "github.com/NpoolPlatform/sphinx-coininfo/pkg/client"
 
@@ -78,24 +79,43 @@ func GetAppDetails(ctx context.Context, appID string, offset, limit int32) ([]*n
 		coinMap[coin.ID] = coin
 	}
 
-	infos := []*npool.Detail{}
+	userIDs := []string{}
 	for _, info := range details {
-		coin, ok := coinMap[info.CoinTypeID]
-		if !ok {
-			return nil, 0, fmt.Errorf("invalid coin")
-		}
+		userIDs = append(userIDs, info.UserID)
+	}
 
+	users, _, err := usermwcli.GetManyUsers(ctx, userIDs)
+	if err != nil {
+		return nil, 0, fmt.Errorf("fail get users: %v", err)
+	}
+	userMap := map[string]*appusermwpb.User{}
+	for _, user := range users {
+		userMap[user.ID] = user
+	}
+
+	infos := []*npool.Detail{}
+	for _, detail := range details {
+		user, ok := userMap[detail.UserID]
+		if !ok {
+			continue
+		}
+		coin, ok := coinMap[detail.CoinTypeID]
+		if !ok {
+			continue
+		}
 		infos = append(infos, &npool.Detail{
-			CoinTypeID: info.CoinTypeID,
-			CoinName:   coin.Name,
-			CoinLogo:   coin.Logo,
-			CoinUnit:   coin.Unit,
-			IOType:     info.IOType,
-			IOSubType:  info.IOSubType,
-			Amount:     info.Amount,
-			IOExtra:    info.IOExtra,
-			CreatedAt:  info.CreatedAt,
-			UserID:     info.UserID,
+			CoinTypeID:   detail.CoinTypeID,
+			CoinName:     coin.Name,
+			CoinLogo:     coin.Logo,
+			CoinUnit:     coin.Unit,
+			IOType:       detail.IOType,
+			IOSubType:    detail.IOSubType,
+			Amount:       detail.Amount,
+			IOExtra:      detail.IOExtra,
+			CreatedAt:    detail.CreatedAt,
+			UserID:       detail.UserID,
+			PhoneNo:      user.PhoneNO,
+			EmailAddress: user.EmailAddress,
 		})
 	}
 
