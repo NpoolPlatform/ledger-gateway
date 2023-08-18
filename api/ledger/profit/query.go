28b733b9d211d8fe226217d1873f38469c83a672
lidaqiang <dqli161@163.com>
2023-08-18 16:24:59 +0800

    add profit api

---
+++ api/ledger/profit/query.go
@@ -0,0 +1,151 @@
+package profit
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/ledger/gw/v1/ledger"
+
+	handler1 "github.com/NpoolPlatform/ledger-gateway/pkg/ledger/handler"
+	profit1 "github.com/NpoolPlatform/ledger-gateway/pkg/ledger/profit"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) GetProfits(ctx context.Context, in *npool.GetProfitsRequest) (*npool.GetProfitsResponse, error) {
+	handler, err := profit1.NewHandler(
+		ctx,
+		handler1.WithAppID(&in.AppID, true),
+		handler1.WithUserID(&in.AppID, &in.UserID, true),
+		handler1.WithOffset(in.Offset),
+		handler1.WithLimit(in.Limit),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetProfits",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetProfitsResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	infos, total, err := handler.GetProfits(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetProfits",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetProfitsResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetProfitsResponse{
+		Infos: infos,
+		Total: total,
+	}, nil
+}
+
+func (s *Server) GetIntervalProfits(ctx context.Context, in *npool.GetIntervalProfitsRequest) (*npool.GetIntervalProfitsResponse, error) {
+	handler, err := profit1.NewHandler(
+		ctx,
+		handler1.WithAppID(&in.AppID, true),
+		handler1.WithUserID(&in.AppID, &in.UserID, true),
+		handler1.WithStartAt(in.StartAt),
+		handler1.WithEndAt(in.EndAt),
+		handler1.WithOffset(in.Offset),
+		handler1.WithLimit(in.Limit),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetIntervalProfits",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetIntervalProfitsResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	infos, total, err := handler.GetIntervalProfits(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetIntervalProfits",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetIntervalProfitsResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetIntervalProfitsResponse{
+		Infos: infos,
+		Total: total,
+	}, nil
+}
+
+func (s *Server) GetGoodProfits(ctx context.Context, in *npool.GetGoodProfitsRequest) (*npool.GetGoodProfitsResponse, error) {
+	handler, err := profit1.NewHandler(
+		ctx,
+		handler1.WithAppID(&in.AppID, true),
+		handler1.WithUserID(&in.AppID, &in.UserID, true),
+		handler1.WithStartAt(in.StartAt),
+		handler1.WithEndAt(in.EndAt),
+		handler1.WithOffset(in.Offset),
+		handler1.WithLimit(in.Limit),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGoodProfits",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodProfitsResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	infos, total, err := handler.GetGoodProfits(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetGoodProfits",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetGoodProfitsResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetGoodProfitsResponse{
+		Infos: infos,
+		Total: total,
+	}, nil
+}
+
+func (s *Server) GetMiningRewards(ctx context.Context, in *npool.GetMiningRewardsRequest) (*npool.GetMiningRewardsResponse, error) {
+	handler, err := profit1.NewHandler(
+		ctx,
+		handler1.WithAppID(&in.AppID, true),
+		handler1.WithUserID(&in.AppID, &in.UserID, true),
+		handler1.WithStartAt(in.StartAt),
+		handler1.WithEndAt(in.EndAt),
+		handler1.WithOffset(in.Offset),
+		handler1.WithLimit(in.Limit),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetMiningRewards",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetMiningRewardsResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	infos, total, err := handler.GetMiningRewards(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetMiningRewards",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetMiningRewardsResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetMiningRewardsResponse{
+		Infos: infos,
+		Total: total,
+	}, nil
+}
--- pkg/ledger/profit/handler.go
+++ pkg/ledger/profit/handler.go
@@ -2,16 +2,12 @@ package profit
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/NpoolPlatform/ledger-gateway/pkg/ledger/handler"
-	basetypes "github.com/NpoolPlatform/message/npool/basetypes/ledger/v1"
 )
 
 type Handler struct {
 	*handler.Handler
-	IOType    *basetypes.IOType
-	IOSubType *basetypes.IOSubType
 }
 
 func NewHandler(ctx context.Context, options ...interface{}) (*Handler, error) {
@@ -33,49 +29,4 @@ func NewHandler(ctx context.Context, options ...interface{}) (*Handler, error) {
 		}
 	}
 	return h, nil
-}
-
-func WithIOtype(_type *basetypes.IOType, must bool) func(context.Context, *Handler) error {
-	return func(ctx context.Context, h *Handler) error {
-		if _type == nil {
-			if must {
-				return fmt.Errorf("invalid io type")
-			}
-			return nil
-		}
-		flag := false
-		for ioType := range basetypes.IOType_value {
-			if ioType == _type.String() && ioType != basetypes.IOType_DefaultType.String() {
-				flag = true
-			}
-		}
-		if !flag {
-			return fmt.Errorf("invalid io type %v", *_type)
-		}
-		h.IOType = _type
-		return nil
-	}
-}
-
-func WithIOSubType(_type *basetypes.IOSubType, must bool) func(context.Context, *Handler) error {
-	return func(ctx context.Context, h *Handler) error {
-		if _type == nil {
-			if must {
-				return fmt.Errorf("invalid io sub type")
-			}
-			return nil
-		}
-
-		flag := false
-		for ioSubType := range basetypes.IOSubType_value {
-			if ioSubType == _type.String() && ioSubType != basetypes.IOSubType_DefaultSubType.String() {
-				flag = true
-			}
-		}
-		if !flag {
-			return fmt.Errorf("invalid io sub type %v", *_type)
-		}
-		h.IOSubType = _type
-		return nil
-	}
 }
