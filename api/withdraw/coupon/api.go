b1b5f8723f270e777281c02a6e0dd2cfeb2c5803
lidaqiang <dqli161@163.com>
2023-12-29 10:22:03 +0800

    change name

--- api/withdraw/coupon/api.go
+++ api/withdraw/coupon/api.go
@@ -3,22 +3,22 @@ package coupon
 import (
 	"context"
 
-	coupon1 "github.com/NpoolPlatform/message/npool/ledger/gw/v1/withdraw/coupon"
+	couponwithdraw "github.com/NpoolPlatform/message/npool/ledger/gw/v1/withdraw/coupon"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
-	coupon1.UnimplementedGatewayServer
+	couponwithdraw.UnimplementedGatewayServer
 }
 
 func Register(server grpc.ServiceRegistrar) {
-	coupon1.RegisterGatewayServer(server, &Server{})
+	couponwithdraw.RegisterGatewayServer(server, &Server{})
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
-	if err := coupon1.RegisterGatewayHandlerFromEndpoint(context.Background(), mux, endpoint, opts); err != nil {
+	if err := couponwithdraw.RegisterGatewayHandlerFromEndpoint(context.Background(), mux, endpoint, opts); err != nil {
 		return err
 	}
 	return nil
