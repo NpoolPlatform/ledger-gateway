9ba8f378f455f8a30a6ad55a94da502efb96f69c
jakys <jakysing@163.com>
2024-06-13 08:16:50 +0000

    Fix http://47.116.78.131:10083/issues/3676

--- pkg/ledger/profit/coinprofit.go
+++ pkg/ledger/profit/coinprofit.go
@@ -4,22 +4,86 @@ import (
 	"context"
 
 	appcoinmwcli "github.com/NpoolPlatform/chain-middleware/pkg/client/app/coin"
+	constant "github.com/NpoolPlatform/ledger-gateway/pkg/const"
 	profitmwcli "github.com/NpoolPlatform/ledger-middleware/pkg/client/ledger/profit"
+	statementcli "github.com/NpoolPlatform/ledger-middleware/pkg/client/ledger/statement"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	ledgertypes "github.com/NpoolPlatform/message/npool/basetypes/ledger/v1"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	appcoinmwpb "github.com/NpoolPlatform/message/npool/chain/mw/v1/app/coin"
 	npool "github.com/NpoolPlatform/message/npool/ledger/gw/v1/ledger/profit"
 	profitmwpb "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger/profit"
+	statementmwpb "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger/statement"
 
 	"github.com/shopspring/decimal"
 )
 
 type coinProfitHandler struct {
 	*Handler
-	infos    []*npool.CoinProfit
-	appCoins []*appcoinmwpb.Coin
-	profits  map[string]*profitmwpb.Profit
-	total    uint32
+	infos       []*npool.CoinProfit
+	appCoins    []*appcoinmwpb.Coin
+	profits     map[string]*profitmwpb.Profit
+	coinTypeIDs []string
+	total       uint32
+}
+
+//nolint:dupl
+func (h *coinProfitHandler) getStatements(ctx context.Context) error {
+	if h.StartAt == nil && h.EndAt == nil {
+		return nil
+	}
+	offset := int32(0)
+	limit := constant.DefaultRowLimit
+	conds := &statementmwpb.Conds{
+		AppID:  &basetypes.StringVal{Op: cruder.EQ, Value: *h.AppID},
+		UserID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.UserID},
+		IOType: &basetypes.Uint32Val{Op: cruder.EQ, Value: uint32(ledgertypes.IOType_Incoming)},
+		IOSubTypes: &basetypes.Uint32SliceVal{Op: cruder.IN, Value: []uint32{
+			uint32(ledgertypes.IOSubType_MiningBenefit),
+			uint32(ledgertypes.IOSubType_SimulateMiningBenefit),
+		}},
+		CoinTypeIDs: &basetypes.StringSliceVal{Op: cruder.IN, Value: h.coinTypeIDs},
+	}
+	if h.StartAt != nil {
+		conds.StartAt = &basetypes.Uint32Val{Op: cruder.EQ, Value: *h.StartAt}
+	}
+	if h.EndAt != nil {
+		conds.EndAt = &basetypes.Uint32Val{Op: cruder.EQ, Value: *h.EndAt}
+	}
+
+	for {
+		statements, _, err := statementcli.GetStatements(ctx, conds, offset, limit)
+		if err != nil {
+			return err
+		}
+		if len(statements) == 0 {
+			break
+		}
+		for _, statement := range statements {
+			profit, ok := h.profits[statement.CoinTypeID]
+			if ok {
+				incoming, err := decimal.NewFromString(profit.Incoming)
+				if err != nil {
+					return err
+				}
+				amount, err := decimal.NewFromString(statement.Amount)
+				if err != nil {
+					return err
+				}
+				profit.Incoming = incoming.Add(amount).String()
+				h.profits[statement.CoinTypeID] = profit
+				continue
+			}
+
+			newProfit := &profitmwpb.Profit{
+				CoinTypeID: statement.CoinTypeID,
+				Incoming:   statement.Amount,
+			}
+			h.profits[statement.CoinTypeID] = newProfit
+		}
+		offset += limit
+	}
+	return nil
 }
 
 func (h *coinProfitHandler) getAppCoins(ctx context.Context) error {
@@ -29,12 +93,20 @@ func (h *coinProfitHandler) getAppCoins(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
+	for _, coin := range coins {
+		h.coinTypeIDs = append(h.coinTypeIDs, coin.CoinTypeID)
+	}
+
 	h.total = total
 	h.appCoins = coins
 	return nil
 }
 
 func (h *coinProfitHandler) getProfits(ctx context.Context) error {
+	if h.StartAt != nil || h.EndAt != nil {
+		return nil
+	}
 	coinTypeIDs := func() (_coinTypeIDs []string) {
 		for _, appCoin := range h.appCoins {
 			_coinTypeIDs = append(_coinTypeIDs, appCoin.CoinTypeID)
@@ -46,12 +118,7 @@ func (h *coinProfitHandler) getProfits(ctx context.Context) error {
 		UserID:      &basetypes.StringVal{Op: cruder.EQ, Value: *h.UserID},
 		CoinTypeIDs: &basetypes.StringSliceVal{Op: cruder.IN, Value: coinTypeIDs},
 	}
-	if h.StartAt != nil {
-		conds.StartAt = &basetypes.Uint32Val{Op: cruder.GTE, Value: *h.StartAt}
-	}
-	if h.EndAt != nil {
-		conds.EndAt = &basetypes.Uint32Val{Op: cruder.LTE, Value: *h.EndAt}
-	}
+
 	profits, _, err := profitmwcli.GetProfits(ctx, conds, 0, int32(len(coinTypeIDs)))
 	if err != nil {
 		return err
@@ -96,6 +163,9 @@ func (h *Handler) GetCoinProfits(ctx context.Context) ([]*npool.CoinProfit, uint
 		return nil, 0, nil
 	}
 	if err := handler.getProfits(ctx); err != nil {
+		return nil, 0, err
+	}
+	if err := handler.getStatements(ctx); err != nil {
 		return nil, 0, err
 	}
 
